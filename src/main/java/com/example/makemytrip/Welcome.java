f1f02c0d83b2d02a98994b3aad5e08364aa7d759
arjun123 <arjun@gmail.com>
2025-05-11 11:11:03 +0530

    1st commit

---
+++ src/main/java/com/example/makemytrip/Welcome.java
@@ -0,0 +1,9 @@
+package com.example.makemytrip;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+    @GetMapping("/")
+    public String getData() {return  "Welcome Devops Course" ; }
+}
